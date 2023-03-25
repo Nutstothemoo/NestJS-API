c7c042b771936323a0f91d08189d9ea4bc83dc65
DUPIN <maximedupin1992@gmail.com>
2023-03-25 11:12:31 +0100

    add userservice

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,21 @@
-import { Body, Controller, Post, Req } from "@nestjs/common/decorators";
+import { HttpStatus } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post, Req } from "@nestjs/common/decorators";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
-@Controller('auth')
-export class AuthController {
-    constructor ( private authService: AuthService){}
-
+  
+  @Controller('auth')
+  export class AuthController {
+    constructor(private authService: AuthService) {}
+  
     @Post('signup')
-    signup(@Body() dto: AuthDto){
-        return this.authService.signup(dto)
+    signup(@Body() dto: AuthDto) {
+      return this.authService.signup(dto);
     }
-
+  
+    @HttpCode(HttpStatus.OK)
     @Post('signin')
-    signin(@Body() dto: AuthDto){
-        return this.authService.signin(dto)
+    signin(@Body() dto: AuthDto) {
+      return this.authService.signin(dto);
     }
-}
+  }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,22 @@
-import { Injectable, Req } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { ForbiddenException } from '@nestjs/common/exceptions';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-    constructor (private prisma: PrismaService){}   
+    constructor (
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService
+        ){}   
 
     async signup(dto: AuthDto){
+
         try {
 
         // hash du mot de passe
@@ -27,9 +34,7 @@ export class AuthService {
             }
         });
 
-        delete user.hash;
-
-        return user;
+        return this.signToken(user.id, user.email)
 
         } catch (error) {
         if (error instanceof PrismaClientKnownRequestError) {
@@ -62,8 +67,24 @@ export class AuthService {
             const passwordcompare = bcrypt.compareSync(dto.password, user.hash);
             if (!passwordcompare) throw new ForbiddenException ('password incorrect')
 
+            return this.signToken(user.id, user.email)
+    }
+    async signToken ( userId: number,  email: string ): Promise<{access_token: string}> {
+    
+        const payload = {
+            sub: userId,
+            email
+        };
 
-            delete user.hash;
-            return user;
+        const secret = this.config.get('JWT_SECRET');
+        
+        const token =await this.jwt.signAsync(payload, {
+            expiresIn: '20m',
+            secret: secret
+        })
+
+        return {
+            access_token: token,
+        };
     }
 }
